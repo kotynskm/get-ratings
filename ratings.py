1cdfffa8bd1b041d0dd02251bc99440e4959fb29
Kailey <k.kotynski@gmail.com>
2022-05-16 16:36:48 -0700

    Added comments

--- ratings.py
+++ ratings.py
@@ -3,8 +3,10 @@
 
 # put your code here
 def get_ratings(file):
-
+    # Opening the file
     open_file = open(file)
+
+    # Adding a new Restaurant rating
     new_rest = input('Restaurant: ').strip().title()
     while True:
         try:
@@ -13,14 +15,18 @@ def get_ratings(file):
             continue    
         if type(rest_rating) == int and 1 <= rest_rating <= 5:
             break
+
+    # Adding the restaurants to a dictionary 
     restaurant_ratings = {new_rest: rest_rating}
     for line in open_file:
         line = line.strip()
         restaurants = line.split(":")
         restaurant_ratings[restaurants[0]] = restaurants[1]
-            
+
+    # Sorting the dictionary     
     sorted_restaurants = sorted(restaurant_ratings.items())
 
+    # Printing all the restaurants
     for rest, rating in sorted_restaurants:
         print(f"{rest} is rated {rating}")
             
