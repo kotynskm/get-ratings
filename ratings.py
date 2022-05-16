5f54e6b74d270980139a72e7188098d377c79412
Kailey <k.kotynski@gmail.com>
2022-05-16 16:51:56 -0700

    Added choices for user

--- ratings.py
+++ ratings.py
@@ -5,32 +5,43 @@
 def get_ratings(file):
     # Opening the file
     open_file = open(file)
-
-    # Adding a new Restaurant rating
-    new_rest = input('Restaurant: ').strip().title()
+    restaurant_ratings = {}
+    # Giving user 3 choices
     while True:
-        try:
-            rest_rating = int(input('Rating: '))
-        except ValueError:
-            continue    
-        if type(rest_rating) == int and 1 <= rest_rating <= 5:
-            break
+        choice = int(input('What would you like to do?\n[1] - See all the ratings\n[2] - Add a new restaurant and rating it\n[3] - Quit '))
+        
+        # Exiting the function
+        if choice == 3:
+            return "Goodbye"
+        # Adding a new Restaurant rating
+        if choice == 2:
+            new_rest = input('Restaurant: ').strip().title()
+            while True:
+                try:
+                    rest_rating = int(input('Rating: '))
+                except ValueError:
+                    continue    
+                if type(rest_rating) == int and 1 <= rest_rating <= 5:
+                    restaurant_ratings = {new_rest: rest_rating}
+                    break
 
-    # Adding the restaurants to a dictionary 
-    restaurant_ratings = {new_rest: rest_rating}
-    for line in open_file:
-        line = line.strip()
-        restaurants = line.split(":")
-        restaurant_ratings[restaurants[0]] = restaurants[1]
+        # Adding the restaurants to a dictionary 
+        if len(restaurant_ratings) == 0:
+            restaurant_ratings = {}
+        if choice == 1:
+            for line in open_file:
+                line = line.strip()
+                restaurants = line.split(":")
+                restaurant_ratings[restaurants[0]] = restaurants[1]
 
-    # Sorting the dictionary     
-    sorted_restaurants = sorted(restaurant_ratings.items())
+            # Sorting the dictionary     
+            sorted_restaurants = sorted(restaurant_ratings.items())
 
-    # Printing all the restaurants
-    for rest, rating in sorted_restaurants:
-        print(f"{rest} is rated {rating}")
+            # Printing all the restaurants and its ratings
+            for rest, rating in sorted_restaurants:
+                print(f"{rest} is rated {rating}")
+                    
             
-    
-    
+        
 
 get_ratings("scores.txt")
